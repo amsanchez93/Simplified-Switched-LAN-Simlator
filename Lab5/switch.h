e3e928ee7a6154a368ad7c4e9eb91e2bc4ca972a
Trevin Goda <treving@hawaii.edu>
2016-03-10 19:11:12 -1000

    added switch header switch.h

---
+++ Lab5/switch.h
@@ -0,0 +1,27 @@
+/* switch.h */
+
+#define TABLE_LENGTH 100
+
+typedef struct {
+	packetBuffer sendPacketBuf;
+	packetBuffer rcvPacketBuf;	
+	switchQueue * head;
+	LinkInfo linkin[NUMHOSTS];
+	LinkInfo linkout[NUMHOSTS];
+	switchTable table;
+} switchState;
+
+typedef struct {
+	packetBuffer data;
+	switchQueue * next;
+} switchQueue;
+
+typedef struct {
+	int valid[TABLE_LENGTH];
+	int dstaddr[TABLE_LENGTH];
+	int outlink[TABLE_LENGTH];
+} switchTable;
+
+void switchMain(switchState * sstate); //main loop for switch
+
+void switchInit(switchState * sstate);
