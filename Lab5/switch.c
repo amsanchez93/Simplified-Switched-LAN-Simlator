daaaa124f1cc29e0748690ff6a140674d5fdf6f7
Trevin Goda <treving@hawaii.edu>
2016-04-07 00:42:28 +0000

    updated switch.c

--- Lab5/switch.c
+++ Lab5/switch.c
@@ -28,20 +28,30 @@ void switchMain(switchState * sstate)
     packetBuffer tempbuff;
     
     while(1){
-        int i,j, rcvSize;
+        int i,j, rcvSize = 0;
         for(i = 0; i < NUMHOSTS; i++){
-            rcvSize = linkReceive(&(sstate->linkin[i]),&(sstate->rcvPacketBuf));
-            if(rcvSize > 0){
+            linkReceive(&(sstate->linkin[i]),&tempbuff);
+            if(tempbuff.valid == 1 && tempbuff.new == 1){
+                sstate->rcvPacketBuf = tempbuff;
                 sstate->rcvPacketBuf.valid = 1;
-                sstate->rcvPacketBuf.new = 1;
-                printf("received packet on link %d", i);
+                printf("received packet on link %d\n", i);
             }
         }
-        
-        for(j = 0; j < NUMHOSTS; j++){
-            if(j != sstate->rcvPacketBuf.srcaddr){
-                linkSend(&(sstate->linkout[j]), &(sstate->sendPacketBuf));
-                printf("sent packet on link %d", j);
+        if(sstate->rcvPacketBuf.valid == 1){
+            sstate->sendPacketBuf.srcaddr = sstate->rcvPacketBuf.srcaddr;
+            sstate->sendPacketBuf.dstaddr = sstate->rcvPacketBuf.dstaddr;
+            sstate->sendPacketBuf.length = sstate->rcvPacketBuf.length;
+            for(i = 0; i < sstate->sendPacketBuf.length;i++){
+                sstate->sendPacketBuf.payload[i] = sstate->rcvPacketBuf.payload[i];
+            }
+            sstate->sendPacketBuf.valid = sstate->rcvPacketBuf.valid;
+            sstate->sendPacketBuf.new = sstate->rcvPacketBuf.new;
+
+            for(j = 0; j < NUMHOSTS; j++){
+                if(j != sstate->sendPacketBuf.srcaddr){
+                    linkSend(&(sstate->linkout[j]), &(sstate->sendPacketBuf));
+                    printf("sent packet on link %d\n", j);
+                }
             }
         }
         usleep(TENMILLISEC);
