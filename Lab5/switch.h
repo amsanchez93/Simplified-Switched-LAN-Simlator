b762a7fc20968a96584d50e52f657cf1725403af
Trevin Goda <treving@hawaii.edu>
2016-04-08 05:11:35 +0000

    switch table

--- Lab5/switch.h
+++ Lab5/switch.h
@@ -10,7 +10,7 @@ typedef struct {
 	struct node * tail;
 	LinkInfo linkin[NUMHOSTS];
 	LinkInfo linkout[NUMHOSTS];
-	//switchTable table;
+	switchTable table;
 } switchState;
 
 typedef struct node{
@@ -18,13 +18,13 @@ typedef struct node{
 	struct node * next;
 } switchNode;
 
-/*
+
 typedef struct {
 	int valid[TABLE_LENGTH];
 	int dstaddr[TABLE_LENGTH];
 	int outlink[TABLE_LENGTH];
 } switchTable;
-*/
+
 void switchMain(switchState * sstate); //main loop for switch
 
 void switchInit(switchState * sstate, int physid);
